24b7ec822edd96c4f0761bdc35a6ba8002bab02a
pedrohcreis <pedroredbox@usp.br>
2020-09-02 13:25:35 -0300

    ajustes de dependencias

--- hackernews-node/src/index.ts
+++ hackernews-node/src/index.ts
@@ -1,22 +1,23 @@
-const { GraphQLServer } = require('graphql-yoga')
+//const { GraphQLServer } = require('graphql-yoga')
+import { GraphQLServer } from 'graphql-yoga';
 
 // 1
 const typeDefs = `
 type Query {
-    info: String!
+  info: String!
 }
 `
 
 // 2
 const resolvers = {
-    Query: {
-        info: () => 'GraphQL Server'
-    }
+  Query: {
+    info: () => 'GraphQL Server'
+  }
 }
 
 // 3
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 })
 server.start(() => console.log('Server is running on http://localhost:4000'))
